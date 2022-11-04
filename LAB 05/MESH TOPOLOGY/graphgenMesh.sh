afd7f7332ac22a16c92bf54af4ed2de4b5e28e91
ashnu416725 <89333666+ashnu416725@users.noreply.github.com>
2022-11-05 01:04:57 +0530

    Create graphgenMesh.sh

---
+++ LAB 05/MESH TOPOLOGY/graphgenMesh.sh
@@ -0,0 +1,10 @@
+set terminal png
+set output 'ResultMesh.png'
+set xrange [0.0:5.0]
+set xlabel "Time(in seconds)"
+set autoscale
+set yrange [0:35]
+set ylabel "Throughput(in Kbps)"
+set grid
+set style data linespoints
+plot "meshthrpt" using 1:2 title "Mesh Throughput"
